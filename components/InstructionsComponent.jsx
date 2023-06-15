b523b985255acc61ab5fc67f09a1531308023931
invblake <84220902+invblake@users.noreply.github.com>
2023-06-15 01:34:14 -0500

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -6,7 +6,7 @@ export default function InstructionsComponent() {
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					create<span>-web3-dapp</span>
+				<span>-web3-dapp</span>
 				</h1>
 				<p>
 					Get started by editing this page in{" "}
